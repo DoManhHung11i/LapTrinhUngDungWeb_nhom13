ee4379d66ad7fbaf61906bcdb93a5304e52c63d2
DoManhHung11i <hung151004@gmail.com>
2024-05-08 23:32:40 +0700

    update đăng nhập

--- src/routes/home.js
+++ src/routes/home.js
@@ -2,15 +2,16 @@ const express = require('express');
 const router = express.Router();
 const HomeController = require('../controllers/homeController');
 const AuthController = require('../controllers/authController');
+const { requireAuth } = require('../middleware/authMiddleware');
 
 
 router.post('/register', AuthController.register);
 router.post('/Login', AuthController.Login);
 router.get('/signup', HomeController.signup);
 router.get('/login', HomeController.login);
-router.get('/Recently', HomeController.Recently);
-router.get('/MyQueue', HomeController.MyQueue);
-router.get('/MyPodcasts', HomeController.MyPodcasts);
+router.get('/Recently',requireAuth , HomeController.Recently);
+router.get('/MyQueue',requireAuth, HomeController.MyQueue);
+router.get('/MyPodcasts', requireAuth, HomeController.MyPodcasts);
 router.get('/Discovery', HomeController.Discovery);
 router.get('/', HomeController.home);
 
